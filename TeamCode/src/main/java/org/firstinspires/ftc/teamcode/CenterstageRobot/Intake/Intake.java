6e49226eee4d770cbb9a3cc8cf20569835fd9a49
Jeff Tipps <jag2k2@gmail.com>
2024-04-10 21:59:57 -0500

    Adding More Intake Tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageRobot/Intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageRobot/Intake/Intake.java
@@ -1,52 +1,97 @@
 package org.firstinspires.ftc.teamcode.CenterstageRobot.Intake;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Intake {
     public ServoState servoState = ServoState.HOLDING;
-    private Servo axonIntake;
+    private Servo axonServoIntake;
+
+    private DcMotorEx intakeRollerMotor;
+
+    private Servo topBucketServo;
+
+    private Servo bottomBucketServo;
+    private boolean triggerWasPressed = false;
+    private boolean fullExtensionWasPressed = false;
+
+    private double startTime = 0;
 
     // constructor, deal with hardware map
-    public Intake(Servo axonIntake) {
-        this.axonIntake = axonIntake;
+    public Intake(Servo axonIntake, DcMotorEx motor, Servo topBucketServo, Servo bottomBucketServo) {
+        this.axonServoIntake = axonIntake;
+        this.intakeRollerMotor = motor;
+        this.topBucketServo = topBucketServo;
+        this.bottomBucketServo = bottomBucketServo;
     }
 
     public void setServoIfValidPosition(double servoPos) {
         if (servoState.equals(ServoState.TO_STACK) && servoPos >= servoAngleToPos(30) && servoPos <= servoAngleToPos(150)) {
-            axonIntake.setPosition(servoPos);
+            axonServoIntake.setPosition(servoPos);
         } else if (servoState.equals(ServoState.TO_GROUND) && servoPos >= servoAngleToPos(30) && servoPos <= servoAngleToPos(180)) {
-            axonIntake.setPosition(servoPos);
+            axonServoIntake.setPosition(servoPos);
         }
     }
 
     public void update(Gamepad gamepad1) {
         switch (servoState) {
             case HOLDING:
-                axonIntake.setPosition(servoAngleToPos(30));
+                axonServoIntake.setPosition(servoAngleToPos(30));
                 if (gamepad1.a) {
                     servoState = ServoState.TO_STACK;
-                    axonIntake.setPosition(servoAngleToPos(150));
                 }
                 break;
             case TO_STACK:
-                if (gamepad1.b) {
-                    servoState = servoState.HOLDING;
+                if (fullExtensionWasPressed == false && gamepad1.b) {
+                    axonServoIntake.setPosition(servoAngleToPos(150));
+                    fullExtensionWasPressed = true;
+                } else if (fullExtensionWasPressed && gamepad1.b) {
+                    axonServoIntake.setPosition(0.0);
+                    fullExtensionWasPressed = false;
                 }
-                if (gamepad1.right_trigger == 1) {
-                    axonIntake.setPosition(servoAngleToPos(175));
-                }
-                if (gamepad1.right_trigger < 1) {
-                    double curPos = axonIntake.getPosition();
-                    if (curPos < servoAngleToPos(175)) {
-                        double updatePos = Math.round((curPos + servoAngleToPos(5)) * 10000d) / 10000d;
-                        axonIntake.setPosition(updatePos);
+                if (gamepad1.a) {
+                    if (axonServoIntake.getPosition() == servoAngleToPos(180)) {
+                        // does this cause the servo to move to 0 degrees? or just sets the direction?
+                        axonServoIntake.setDirection(Servo.Direction.REVERSE);
+                        axonServoIntake.setPosition(0);
+                        double pos = 0.0;
+                        pos = pos + servoAngleToPos(10);
+                        axonServoIntake.setPosition(pos);
+                    } else {
+                        double pos = axonServoIntake.getPosition();
+                        System.out.println(pos);
+                        pos = pos + servoAngleToPos(10);
+                        axonServoIntake.setPosition(pos);
                     }
                 }
                 break;
+            case TO_GROUND:
         }
+
+        if (gamepad1.right_trigger >= 0.5f) {
+            topBucketServo.setPosition(0);
+            bottomBucketServo.setPosition(0);
+            intakeRollerMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            intakeRollerMotor.setPower(1);
+            triggerWasPressed = true;
+        } else if (triggerWasPressed && gamepad1.right_trigger < 0.5f) {
+            axonServoIntake.setPosition(servoAngleToPos(30));
+            triggerWasPressed = false;
+            intakeRollerMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            runMotorFor2Seconds();
+        }
+    }
+
+    public void runMotorFor2Seconds() {
+        startTime = System.currentTimeMillis();
+        System.out.println(startTime);
+        while ((System.currentTimeMillis() - startTime >= 1900) && (System.currentTimeMillis() - startTime <= 2200)) {
+            intakeRollerMotor.setPower(1);
+        }
+        intakeRollerMotor.setPower(0);
     }
 
     public enum ServoState {
@@ -61,7 +106,7 @@ public class Intake {
         }
         // turns desired angle into a 0-1 value servo understands
         double pos = 180/angle;
-        pos = Math.round((1/pos)*10000d)/10000d;
+        pos = 1/pos;
         return pos;
     }
 
--- TeamCode/src/test/java/centerstageTests/IntakeTests.java
+++ TeamCode/src/test/java/centerstageTests/IntakeTests.java
@@ -9,28 +9,59 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 // TODO List:
-// Motor continuously spins rollers
+// -- Questions --
+// Trigger and button functionality the same for both the TO_STACK and TO_GROUND state?
+// Do you want a button that switches the state back to HOLDING?
+
+// -- Functionality --
+// Motor continuously spins rollers (this is turned on by a trigger) -> DONE
+// Button a -> When clicked, servo should increase pos by 10 degrees, click multiple times to get to full pos (180) -> DONE
+    // When it reaches the lowest pos, cycle back up by 10 to starting, used for stacks (pos doesn't save btw intake cycles) -> KIND OF DONE
+// Button b -> When toggled on, should go to full pos (180) --> DONE
+    // when pressed again, should go back to top -> DONE
+// Trigger -> Treat like a button, turns on intaking rollers if pressed, once trigger is released, should go back to
+    // init pos -> DONE
+    // reverse for 2 seconds
+    // on & off values: >= 0.5 -> DONE
+    // When turned on, should reset the direction of the roller motor to be forward -> DONE
+    // if want to go back to intaking, should not have to wait for seconds to be over
+    // When this is pressed to turn on intake, servos on bucket should reset to pos 0 -> DONE
+
+// -- Refactoring --
 // Find a way to verify the enum without a public variable
 // Add servo range to IntakeConstants?
-// Goes in reverse for 2 secs (TO_STACK & GROUND?)
-    // intake should reverse for 2 seconds after completing intaking, but if need to intake again, shouldn't have to wait
-// Ensure that the right trigger goes to pos when clicked once, not when held down
-// Maybe create two states, INTAKING, OUTTAKING, and HOLDING, and then within INTAKING have TO_STACK and TO_GROUND
+// Figure out rounding, nicer, but not totally necessary
+// Copy Alex's Toggle class for the buttons & GamepadMapping
+// Get rid of the TO_STACK anf TO_GROUND states, only do Intakeing, Holding, and Outtackeing
+// BREAK STUFF UP INTO METHODS GOOBER :)
 
 @ExtendWith(MockitoExtension.class)
 public class IntakeTests {
     @Mock
     Servo axonServo;
+    @Mock
+    DcMotorEx intakeRollerMotor;
+
+    @Mock
+    Servo topBucketServo;
+
+    @Mock
+    Servo bottomBucketServo;
 
     private Intake intake;
+    private Gamepad gamepad1;
 
     @BeforeEach
     public void setUp() {
-        intake = new Intake(axonServo);
+        intake = new Intake(axonServo, intakeRollerMotor, topBucketServo, bottomBucketServo);
+        gamepad1 = new Gamepad();
     }
     @Test
     public void testThatServoOnlyMoves30To150ToStack() {
@@ -41,7 +72,7 @@ public class IntakeTests {
     }
 
     @Test
-    public void testThatServoWontMoveIfPosOutOfRange() {
+    public void testThatServoWontMoveIfPosOutOfRangeToStack() {
         double servoPos = Intake.servoAngleToPos(180);
         intake.servoState = Intake.ServoState.TO_STACK;
         intake.setServoIfValidPosition(servoPos);
@@ -59,67 +90,81 @@ public class IntakeTests {
     @Test
     public void servoCanSwitchModes() {
         intake.servoState = Intake.ServoState.HOLDING;
-        Gamepad gamepad1 = new Gamepad();
         gamepad1.a = true;
         intake.update(gamepad1);
         assertEquals(Intake.ServoState.TO_STACK, intake.servoState);
     }
 
+//    @Test
+//    public void servoCanSwitchBackToHolding() {
+//        intake.servoState = Intake.ServoState.TO_STACK;
+//        Gamepad gamepad1 = new Gamepad();
+//        gamepad1.b = true;
+//        intake.update(gamepad1);
+//        assertEquals(Intake.ServoState.HOLDING, intake.servoState);
+//    }
+
     @Test
-    public void servoCanSwitchBackToHolding() {
+    public void servoGoesToFullStackPosWhenButtonClicked() {
         intake.servoState = Intake.ServoState.TO_STACK;
-        Gamepad gamepad1 = new Gamepad();
+
         gamepad1.b = true;
         intake.update(gamepad1);
-        assertEquals(Intake.ServoState.HOLDING, intake.servoState);
+
+        verify(axonServo).setPosition(eq(Intake.servoAngleToPos(150)));
     }
+
     @Test
-    public void servoGoesToFullPosWhenTriggerClicked() {
-        Gamepad gamepad1 = new Gamepad();
+    public void servoGoesToStartingPosWhenButtonClickedAgain() {
         intake.servoState = Intake.ServoState.TO_STACK;
 
-        gamepad1.right_trigger = 1;
+        gamepad1.b = true;
+        intake.update(gamepad1);
         intake.update(gamepad1);
 
-        verify(axonServo).setPosition(eq(Intake.servoAngleToPos(175)));
+
+        verify(axonServo).setPosition(eq(0.0));
     }
 
     @Test
-    public void servoMovesInSmallIncsTo175() {
-        Gamepad gamepad1 = new Gamepad();
+    public void servoCanMoveToFullStackPosWhenButtonClickedMultipleTimes() {
+        gamepad1 = new Gamepad();
         intake.servoState = Intake.ServoState.TO_STACK;
-        when(axonServo.getPosition()).thenReturn(Intake.servoAngleToPos(150));
-        double updatePos = axonServo.getPosition();
+        when(axonServo.getPosition()).thenReturn(Intake.servoAngleToPos(130));
 
-        for (float i = .1f; i <= .5f; i += .1f) {
-            when(axonServo.getPosition()).thenReturn(updatePos);
-            gamepad1.right_trigger = i;
-            intake.update(gamepad1);
-            updatePos = updatePos + Intake.servoAngleToPos(5);
-        }
-        verify(axonServo).setPosition(Intake.servoAngleToPos(150) + Intake.servoAngleToPos(5)*5);
+        gamepad1.a = true;
+        intake.update(gamepad1);
+
+        double updatePos = axonServo.getPosition() + Intake.servoAngleToPos(10);
+        when(axonServo.getPosition()).thenReturn(updatePos);
+
+        intake.update(gamepad1);
+
+        verify(axonServo).setPosition(eq(Intake.servoAngleToPos(150)));
     }
 
+    // TODO: Test Servo Behavior in MakerSpace
     @Test
-    public void servoMovesInSmallIncsNotPast175() {
-        Gamepad gamepad1 = new Gamepad();
+    public void servoMovesDownToLowestPosAndThenBackUpInIncrementsOfButtonClick() {
+        gamepad1 = new Gamepad();
         intake.servoState = Intake.ServoState.TO_STACK;
-        when(axonServo.getPosition()).thenReturn(Intake.servoAngleToPos(150));
-        double updatePos = axonServo.getPosition();
+        when(axonServo.getPosition()).thenReturn(Intake.servoAngleToPos(170));
 
-        for (float i = .1f; i <= .6f; i += .1f) {
-            when(axonServo.getPosition()).thenReturn(updatePos);
-            gamepad1.right_trigger = i;
-            intake.update(gamepad1);
-            updatePos = updatePos + Intake.servoAngleToPos(5);
-        }
-        verify(axonServo, times(5)).setPosition(anyDouble());
+        gamepad1.a = true;
+        intake.update(gamepad1); // moves it to 180
+
+        double updatePos = axonServo.getPosition() + Intake.servoAngleToPos(10);
+        when(axonServo.getPosition()).thenReturn(updatePos);
+
+        intake.update(gamepad1); // this should make servo go back up
+
+        verify(axonServo).setPosition(eq(Intake.servoAngleToPos(10)));
     }
 
     @Test
     public void testServoAngleToPos() {
         double pos = Intake.servoAngleToPos(150);
-        double expected = 0.8333;
+        double expected = 0.8333333333333334;
         assertEquals(expected, pos);
     }
 
@@ -128,5 +173,82 @@ public class IntakeTests {
         double pos = Intake.servoAngleToPos(0);
         double expected = 0.0;
         assertEquals(expected, pos);
+    }
+
+    @Test
+    public void whenTriggerPressedRollerMotorTurnsOn() {
+        gamepad1.right_trigger = .6f;
+        intake.update(gamepad1);
+        verify(intakeRollerMotor).setPower(anyDouble());
+    }
+
+    @Test
+    public void whenTriggerNotPressedMotorIsOff() {
+        gamepad1.right_trigger = .2f;
+        intake.update(gamepad1);
+        verify(intakeRollerMotor, never()).setPower(anyDouble());
+    }
+
+    @Test
+    public void whenTriggerPressedMotorModeIsForward() {
+        gamepad1.right_trigger = .5f;
+        intake.update(gamepad1);
+        verify(intakeRollerMotor).setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    @Test
+    public void whenTriggerPressedBucketServosResetTo0() {
+        gamepad1.right_trigger = 1f;
+        intake.update(gamepad1);
+        verify(topBucketServo).setPosition(0);
+        verify(bottomBucketServo).setPosition(0);
+    }
+
+    @Test
+    public void whenTriggerReleasedAxonServoMovesToStartingPos() {
+        intake.servoState = Intake.ServoState.TO_STACK;
+        gamepad1.right_trigger = .5f;
+        intake.update(gamepad1);
+        gamepad1.right_trigger = 0f;
+        intake.update(gamepad1);
+        verify(axonServo, times(1)).setPosition(Intake.servoAngleToPos(30));
+    }
+
+    @Test
+    public void whenTriggerIsNotPressedAndWasNotJustPressedServoDoesNotMove() {
+        intake.servoState = Intake.ServoState.TO_STACK;
+        gamepad1.right_trigger = 0.6f;
+        intake.update(gamepad1);
+
+        // pos should be set to starting only here
+        gamepad1.right_trigger = 0f;
+        intake.update(gamepad1);
+
+        gamepad1.right_trigger = 0f;
+        intake.update(gamepad1);
+        verify(axonServo, times(1)).setPosition(anyDouble());
+    }
+
+    @Test
+    public void whenTriggerReleasedRollerMotorGoesInReverse() {
+        // trigger pushed
+        gamepad1.right_trigger = 0.6f;
+        intake.update(gamepad1);
+        // trigger released
+        gamepad1.right_trigger = 0f;
+        intake.update(gamepad1);
+
+        verify(intakeRollerMotor).setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Test
+    public void whenTriggerReleasedRollerMotorReversesFor2Secs() {
+        // timestamp, when motor starts, when motor stops, subtract to see if in range to verify
+        double startTime = System.currentTimeMillis();
+        intake.runMotorFor2Seconds();
+        double endTime = System.currentTimeMillis();
+        double elapsedTime = endTime - startTime;
+        System.out.println(elapsedTime);
+        assertTrue(elapsedTime <= 2100 && elapsedTime >= 1900);
     }
 }
