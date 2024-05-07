ec2728b441dbc4ef2a6b76615f2660854abf11c5
Jeff Tipps <jag2k2@gmail.com>
2024-05-07 17:23:18 -0500

    restructured the opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/randomTests/AxonServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/randomTests/AxonServoTest.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.randomTests;
 
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+@TeleOp
 public class AxonServoTest extends LinearOpMode {
     ServoEx axonServo;
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/randomTests/MotorEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/randomTests/MotorEncoderTest.java
@@ -1,26 +1,31 @@
 package org.firstinspires.ftc.teamcode.randomTests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.util.Encoder;
 
-public class MotorEncoderTest extends LinearOpMode {
+@TeleOp
+public class MotorEncoderTest extends OpMode {
     DcMotorEx motor;
     Encoder encoder;
-    @Override
-    public void runOpMode() throws InterruptedException {
-        motor = hardwareMap.get(DcMotorEx.class, "motor");
-        encoder = new Encoder(motor);
 
-        while (opModeIsActive()) {
-            telemetry.addData("Motor pos (w/o encoder)", motor.getCurrentPosition());
-            telemetry.addData("Motor pos (w/ encoder)", encoder.getCurrentPosition());
-            telemetry.addData("Motor velocity (w/o encoder)", motor.getVelocity(AngleUnit.DEGREES));
-            telemetry.addData("Motor raw velocity (w/ encoder)", encoder.getRawVelocity());
-            telemetry.addData("Motor corrected velocity (w/ encoder)", encoder.getCorrectedVelocity());
-        }
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotorEx.class, "leftFront");
+        encoder = new Encoder(motor);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Motor pos (w/o encoder)", motor.getCurrentPosition());
+        telemetry.addData("Motor pos (w/ encoder)", encoder.getCurrentPosition());
+        telemetry.addData("Motor velocity (w/o encoder)", motor.getVelocity(AngleUnit.DEGREES));
+        telemetry.addData("Motor raw velocity (w/ encoder)", encoder.getRawVelocity());
+        telemetry.addData("Motor corrected velocity (w/ encoder)", encoder.getCorrectedVelocity());
     }
 }
